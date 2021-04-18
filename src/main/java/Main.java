f254a14c20319431ba0921c7bd4d28a07141bf9a
hogooo <hosseingoli8899@gmail.com>
2021-04-18 19:19:01 +0430

    added gitIgnore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import java.net.Socket;
 
+public class Main{
+    public static void main(String[] args) {
+        int x = 3;
+        int y = Integer.parseInt("323");
+        Integer z = x + y;
+        System.out.println(z);
     }
 }
