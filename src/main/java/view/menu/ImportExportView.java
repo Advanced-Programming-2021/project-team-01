f2dc49ac69cad3c44fcbb4c904ee90c37ee7cb59
hogooo <hosseingoli8899@gmail.com>
2021-04-21 22:35:29 +0430

    added ProfileView

--- src/main/java/view/menu/ImportExportView.java
+++ src/main/java/view/menu/ImportExportView.java
@@ -4,26 +4,33 @@ import com.google.gson.Gson;
 
 import java.util.regex.Matcher;
 
-class ImportExportView
-{
-    
-    public void run(String input) 		
-    {
-        
-    }		
-    
-    private void importCard(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void exportCard(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private Gson gsonBuilder(Matcher matcher)
-    {
+class ImportExportView {
+
+    public void run(String input) {
+
+    }
+
+    private void importCard(Matcher matcher) {
+
+    }
+
+    private void exportCard(Matcher matcher) {
+
+    }
+
+    private void exitMenu(){
+
+    }
+
+    private void showCurrentMenu(){
+
+    }
+
+    private void enterMenu(Matcher matcher){
+
+    }
+
+    private Gson gsonBuilder(Matcher matcher) {
         return null;
-    }		
+    }
 }
