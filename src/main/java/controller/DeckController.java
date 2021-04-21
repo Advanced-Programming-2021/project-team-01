1d2cce9f8bfed4b48f530d2edee1e63f0d26229d
dadbeh123 <dadbeh123@gmail.com>
2021-04-21 09:25:27 +0430

    Deck section updated

--- src/main/java/controller/DeckController.java
+++ src/main/java/controller/DeckController.java
@@ -1,45 +1,44 @@
 package controller;
 
- class DeckController
-{
-    
-    public void createDeck(String name) 		
-    {
-        
-    }		
-    
-    public void deleteDeck(String name) 		
-    {
-        
-    }		
-    
-    public void activateDeck(String name) 		
-    {
-        
-    }		
-    
-    public void addCardToDeck(String cardName, String deckName, boolean isMainDeck) 		
-    {
-        
-    }		
-    
-    public void removeCardFromDeck(String cardName, String deckName, boolean isMainDeck) 		
-    {
-        
-    }		
-    
-    public void showAllDecks() 		
-    {
-        
-    }		
-    
-    public void showDeckByName(String name, boolean isMainDeck)
-    {
-        
-    }		
-    
-    public void showPlayersAllCards() 		
-    {
-        
-    }		
+public class DeckController {
+    private static DeckController instance = null;
+
+    public static DeckController getInstance() {
+        if (instance == null) {
+            instance = new DeckController();
+        }
+        return instance;
+    }
+
+    public void createDeck(String name) {
+
+    }
+
+    public void deleteDeck(String name) {
+
+    }
+
+    public void activateDeck(String name) {
+
+    }
+
+    public void addCardToDeck(String cardName, String deckName, boolean isMainDeck) {
+
+    }
+
+    public void removeCardFromDeck(String cardName, String deckName, boolean isMainDeck) {
+
+    }
+
+    public void showAllDecks() {
+
+    }
+
+    public void showDeckByName(String name, boolean isMainDeck) {
+
+    }
+
+    public void showPlayersAllCards() {
+
+    }
 }
--- src/main/java/view/menu/DeckView.java
+++ src/main/java/view/menu/DeckView.java
@@ -1,52 +1,96 @@
 package view.menu;
 
+import controller.DeckController;
+import view.ConsoleCommands;
+
 import java.util.regex.Matcher;
 
-class DeckView
-{
-    
-    public void run(String input) 		
-    {
-        
-    }		
-    
-    private void createDeck(Matcher matcher)
-    {
-        
-    }		
-    
-    private void deleteDeck(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void activateDeck(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void addCardToDeck(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void removeCardFromDeck(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void showDeck(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void showAllDeck() 		
-    {
-        
-    }		
-    
-    private void showDecksCards(Matcher matcher) 		
-    {
-        
-    }		
+public class DeckView {
+    public void run(String input) {
+        Matcher matcher;
+        if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.CREATE_DECK, input)) != null) {
+            createDeck(matcher);
+        } else if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.DELETE_DECK, input)) != null) {
+            deleteDeck(matcher);
+        } else if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.ACTIVATE_DECK, input)) != null) {
+            activateDeck(matcher);
+        } else if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.ADD_CARD, input)) != null) {
+            addCardToDeck(matcher);
+        } else if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.REMOVE_CARD, input)) != null) {
+            removeCardFromDeck(matcher);
+        } else if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.SHOW_DECK, input)) != null) {
+            showDeck(matcher);
+        } else if (ConsoleCommands.getMatcher(ConsoleCommands.SHOW_ALL_DECK, input) != null) {
+            showAllDeck();
+        } else if (ConsoleCommands.getMatcher(ConsoleCommands.SHOW_ALL_CARDS, input) != null) {
+            showDecksCards();
+        } else
+            System.out.println("invalid command");
+    }
+
+    private void createDeck(Matcher matcher) {
+        String name = matcher.group("name");
+        try {
+            DeckController.getInstance().createDeck(name);
+            System.out.println("deck created successfully!");
+        } catch (Exception expt) {
+            System.out.println(expt.getMessage());
+        }
+    }
+
+    private void deleteDeck(Matcher matcher) {
+        String name = matcher.group("name");
+        try {
+            DeckController.getInstance().deleteDeck(name);
+            System.out.println("deck deleted successfully");
+        } catch (Exception expt) {
+            System.out.println(expt.getMessage());
+        }
+    }
+
+    private void activateDeck(Matcher matcher) {
+        String name = matcher.group("name");
+        try {
+            DeckController.getInstance().activateDeck(name);
+            System.out.println("deck activated successfully");
+        } catch (Exception expt) {
+            System.out.println(expt.getMessage());
+        }
+    }
+
+    private void addCardToDeck(Matcher matcher) {
+        String cardName = matcher.group("cardName"), deckName = matcher.group("deckName");
+        boolean isMainDeck = matcher.group("side").equals("");
+
+        try {
+            DeckController.getInstance().addCardToDeck(cardName, deckName, isMainDeck);
+            System.out.println("card added to deck successfully");
+        } catch (Exception expt) {
+            System.out.println(expt.getMessage());
+        }
+    }
+
+    private void removeCardFromDeck(Matcher matcher) {
+        String cardName = matcher.group("cardName"), deckName = matcher.group("deckName");
+        boolean isMainDeck = matcher.group("side").equals("");
+
+        try {
+            DeckController.getInstance().removeCardFromDeck(cardName, deckName, isMainDeck);
+            System.out.println("card removed from deck successfully");
+        } catch (Exception expt) {
+            System.out.println(expt.getMessage());
+        }
+    }
+
+    private void showDeck(Matcher matcher) {
+
+    }
+
+    private void showAllDeck() {
+
+    }
+
+    private void showDecksCards() {
+
+    }
 }
