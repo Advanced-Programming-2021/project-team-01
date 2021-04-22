6789fe5507008a0dac7fb397d3eaa1fab68abc8d
dadbeh123 <dadbeh123@gmail.com>
2021-04-22 19:10:16 +0430

    DeckView updated

--- src/main/java/view/menu/DeckView.java
+++ src/main/java/view/menu/DeckView.java
@@ -1,12 +1,14 @@
 package view.menu;
 
 import controller.DeckController;
+import controller.exceptions.InvalidMenuNavigation;
 import model.Deck;
 import model.card.Card;
 import model.card.MonsterCard;
 import model.card.SpellCard;
 import model.card.TrapCard;
 import view.ConsoleCommands;
+import view.Menu;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -31,8 +33,34 @@ public class DeckView {
             showAllDeck();
         } else if (ConsoleCommands.getMatcher(ConsoleCommands.SHOW_ALL_CARDS, input) != null) {
             showDecksCards();
+        } else if ((matcher = ConsoleCommands.getMatcher(ConsoleCommands.MENU_ENTER, input)) != null) {
+            try {
+                enterMenu(matcher);
+            } catch (InvalidMenuNavigation expt) {
+                System.out.println(expt.getMessage());
+            }
+        } else if (ConsoleCommands.getMatcher(ConsoleCommands.MENU_EXIT, input) != null) {
+            exitMenu();
+        } else if (ConsoleCommands.getMatcher(ConsoleCommands.MENU_SHOW_CURRENT, input) != null) {
+            showCurrentMenu();
         } else
             System.out.println("invalid command");
+    }
+
+    private void enterMenu(Matcher matcher) throws InvalidMenuNavigation {
+        String menuName = matcher.group("menu");
+        if (Menu.getMenu(menuName) == Menu.MAIN_MENU)
+            HandleRequestType.currentMenu = Menu.MAIN_MENU;
+        else
+            throw new InvalidMenuNavigation();
+    }
+
+    private void exitMenu() {
+        HandleRequestType.currentMenu = Menu.MAIN_MENU;
+    }
+
+    private void showCurrentMenu() {
+        System.out.println("Deck Menu");
     }
 
     private void createDeck(Matcher matcher) {
