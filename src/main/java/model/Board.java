c0d53ae0272b48486327f16fc0d42ff9ec050236
hogooo <hosseingoli8899@gmail.com>
2021-04-23 19:56:51 +0430

    board update

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 
 public class Board
 {
-    private FieldZoneSlot playerOneFieldZone;		
-    private FieldZoneSlot playerTwoFieldZone;		
+    private ZoneSlot[] playerOneMonsterZone;
+    private ZoneSlot[] playerTwoMonsterZone;
+    private ZoneSlot[] playerOneSpellZone;
+    private ZoneSlot[] playerTwoSpellZone;
     private ArrayList<Card> playerOneGraveYard;
     private ArrayList<Card> playerTwoGraveYard;
     private ArrayList<Card> playerOneBanishedZone;		
@@ -15,5 +17,15 @@ public class Board
     private Player playerOne;		
     private Player playerTwo;		
     private ArrayList<Card> playerOneDrawZone;		
-    private ArrayList<Card> playerTwoDrawZone;		
+    private ArrayList<Card> playerTwoDrawZone;
+    {
+        playerOneMonsterZone = new ZoneSlot[6];
+        playerOneBanishedZone = new ArrayList<>();
+        playerOneSpellZone = new ZoneSlot[6];
+        playerTwoMonsterZone = new ZoneSlot[6];
+        playerTwoBanishedZone = new ArrayList<>();
+        playerTwoSpellZone = new ZoneSlot[6];
+        playerOneGraveYard = new ArrayList<>();
+        playerTwoGraveYard = new ArrayList<>();
+    }
 }
