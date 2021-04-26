4dc916f1edcb7e2033bf60f089b6954985466342
dadbeh123 <dadbeh123@gmail.com>
2021-04-26 10:36:02 +0430

    Main exceptions added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import com.opencsv.exceptions.CsvValidationException;
 import controller.DatabaseController;
 import view.menu.HandleRequestType;
 
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, CsvValidationException {
         DatabaseController.loadGameCards();
         HandleRequestType handleRequestType = new HandleRequestType();
         handleRequestType.start();
