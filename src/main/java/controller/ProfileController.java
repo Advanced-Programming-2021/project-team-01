5f2804a756095fe4b76899a6a7d0324a7a50f763
Erfan Sadraiye <sadraiyeh.e2002@gmail.com>
2021-05-01 18:13:36 +0430

    ProfileController updated

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import controller.exceptions.InvalidPassword;
 import controller.exceptions.NicknameExists;
+import controller.exceptions.SameOldNewPassword;
 
 public class ProfileController {
     private static ProfileController instance = null;
@@ -18,7 +20,11 @@ public class ProfileController {
         RegisterController.getInstance().onlineUser.setNickname(nickname);
     }
 
-    public void changePassword(String currentPassword, String newPassword) {
-
+    public void changePassword(String currentPassword, String newPassword) throws Exception {
+        if (!RegisterController.getInstance().onlineUser.getPassword().equals(currentPassword))
+            throw new InvalidPassword();
+        if (currentPassword.equals(newPassword))
+            throw new SameOldNewPassword();
+        RegisterController.getInstance().onlineUser.setPassword(newPassword);
     }
 }
