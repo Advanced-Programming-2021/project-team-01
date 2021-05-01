ca4fe976875bce609eeeb9027c938c6b961c3376
hogooo <hosseingoli8899@gmail.com>
2021-05-01 17:45:30 +0430

    TEST

--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -1,24 +1,20 @@
 package controller;
 
-public class ProfileController
-{
+public class ProfileController {
     private static ProfileController instance = null;
-    
-    public static ProfileController getInstance()
-    {
-        if (instance == null){
+
+    public static ProfileController getInstance() {
+        if (instance == null) {
             instance = new ProfileController();
         }
         return instance;
-    }		
+    }
 
-    public void changeNickname(String nickname) 		
-    {
-        
-    }		
-    
-    public void changePassword(String currentPassword, String newPassword) 		
-    {
-        
-    }		
+    public void changeNickname(String nickname) {
+        System.out.println("EOOOOOO");
+    }
+
+    public void changePassword(String currentPassword, String newPassword) {
+
+    }
 }
