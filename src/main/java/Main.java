7ad74ab2006ab5cd6134e815b1ce61beaa9eace2
dadbeh123 <dadbeh123@gmail.com>
2021-05-07 15:23:46 +0430

    ImportExport added and tested

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import com.opencsv.exceptions.CsvValidationException;
 import controller.DatabaseController;
+import controller.exceptions.CardNameNotExists;
 import view.menu.HandleRequestType;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, CsvValidationException {
+    public static void main(String[] args) throws IOException, CsvValidationException, CardNameNotExists {
         DatabaseController.loadGameCards();
         HandleRequestType handleRequestType = new HandleRequestType();
         handleRequestType.start();
