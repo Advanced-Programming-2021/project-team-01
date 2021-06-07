918d239a78996c81d15adfc146a41c242a6763d7
hogooo <hosseingoli8899@gmail.com>
2021-06-07 16:49:26 +0430

    profile completed

--- src/main/java/view/ProfileView.java
+++ src/main/java/view/ProfileView.java
@@ -1,16 +1,54 @@
 package view;
 
 import controller.DatabaseController;
+import controller.ProfileController;
 import controller.RegisterController;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
 
 public class ProfileView {
     public void changeNickname(MouseEvent event) {
-
+        TextInputDialog inputDialog = new TextInputDialog();
+        inputDialog.setHeaderText("enter new nickname");
+        inputDialog.showAndWait();
+        String nickname = inputDialog.getEditor().getText();
+        if (nickname.isBlank()){
+            inputDialog.close();
+            new MyAlert(Alert.AlertType.ERROR,"emptyField").show();
+            return;
+        }
+        try {
+            ProfileController.getInstance().changeNickname(nickname);
+        } catch (Exception exception) {
+            System.err.println(exception.getMessage());
+        }
     }
 
     public void changePassword(MouseEvent event) {
-
+        TextInputDialog inputDialog = new TextInputDialog();
+        inputDialog.setHeaderText("enter your old password");
+        inputDialog.showAndWait();
+        String oldPassword = inputDialog.getEditor().getText();
+        if (oldPassword.isBlank()){
+            inputDialog.close();
+            new MyAlert(Alert.AlertType.ERROR,"emptyField").show();
+            return;
+        }
+        inputDialog.close();
+        inputDialog.setContentText("enter your new password");
+        inputDialog.showAndWait();
+        String newPassword = inputDialog.getEditor().getText();
+        if (newPassword.isBlank()){
+            inputDialog.close();
+            new MyAlert(Alert.AlertType.ERROR,"emptyField").show();
+            return;
+        }
+        try {
+            ProfileController.getInstance().changePassword(oldPassword, newPassword);
+        } catch (Exception exception) {
+            new MyAlert(Alert.AlertType.ERROR,exception.getMessage()).show();
+        }
 
     }
 
