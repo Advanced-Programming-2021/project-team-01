767a47950bed5eb50b46cf246d335d213c226623
dadbeh123 <dadbeh123@gmail.com>
2021-06-12 13:46:08 +0430

    Deck preview renewed

--- src/main/java/view/DeckPreView.java
+++ src/main/java/view/DeckPreView.java
@@ -1,8 +1,12 @@
 package view;
 
 import controller.DatabaseController;
+import controller.DeckController;
 import controller.RegisterController;
+import controller.exceptions.DeckNotExists;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 
 import java.net.URL;
@@ -21,5 +25,28 @@ public class DeckPreView implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         init();
+    }
+
+    @FXML
+    private void exit() {
+        ViewSwitcher.switchTo(View.MAIN);
+    }
+
+    @FXML
+    private void deleteDeck() throws DeckNotExists {
+        if (deckBar.getValue() == null) {
+            new MyAlert(Alert.AlertType.ERROR, "No deck is selected.").show();
+        }
+        DeckController.getInstance().deleteDeck(deckBar.getValue());
+        deckBar.getItems().remove(deckBar.getValue());
+        new MyAlert(Alert.AlertType.CONFIRMATION, "Deck is deleted.").show();
+    }
+
+    @FXML
+    private void createDeck() {
+        if (deckBar.getValue() == null) {
+            new MyAlert(Alert.AlertType.ERROR, "No deck is selected.").show();
+        }
+
     }
 }
