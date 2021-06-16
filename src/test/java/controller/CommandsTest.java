02bb78ffc0c51cafffa501c22c8891b32a337ec7
Erfan Sadraiye <sadraiyeh.e2002@gmail.com>
2021-06-16 23:47:46 +0430

    commit

---
+++ src/test/java/controller/CommandsTest.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CommandsTest {
+    @Test
+    @DisplayName("")
+    void test1(){
+
+    }
+
+}
