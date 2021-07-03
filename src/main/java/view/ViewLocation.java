0fbf74a7cc096081d41b7526892245827c0b3916
hogooo <hosseingoli8899@gmail.com>
2021-07-03 11:53:52 +0430

    context update

---
+++ src/main/java/view/ViewLocation.java
@@ -0,0 +1,11 @@
+package view;
+
+public enum ViewLocation {
+    MONSTER_OFFENSIVE,
+    MONSTER_DEFENSIVE,
+    SPELL_HIDDEN,
+    SPELL_ACTIVATED,
+    HAND_MONSTER_NORMAL,
+    HAND_SPELL,
+    HAND_MONSTER_EFFECT
+}
