bb8bdcdf8ff2bd30adf5c651c0a869f215df3d20
hogooo <hosseingoli8899@gmail.com>
2021-07-07 18:50:54 +0430

    login size

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,6 +15,6 @@ public class Main extends Application {
         DatabaseController.loadGameCards();
         GameController.getInstance();
         ViewSwitcher.setStage(primaryStage);
-        ViewSwitcher.switchTo(View.MAIN);
+        ViewSwitcher.switchTo(View.LOGIN);
     }
 }
