47d50de0128fe1d3fb4989e98d20637e2c9a6789
dadbeh123 <dadbeh123@gmail.com>
2021-07-15 14:54:08 +0430

    Chat view is updated

--- src/main/java/view/ChatView.java
+++ src/main/java/view/ChatView.java
@@ -1,16 +1,30 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class ChatView implements GraphicalView {
     public TextArea messageField;
+    public ScrollPane scrollPane;
+    public VBox messagesVBox;
 
     @Override
     public void init(Pane root) {
+    }
 
+    @FXML
+    private void sendMessage() {
+        if (messageField.getText().length() > 0) {
+            Label label = new Label(messageField.getText());
+            label.setStyle("-fx-text-fill: white;-fx-font: 16px \"Arial\";");
+            messagesVBox.getChildren().add(label);
+            messageField.setText("");
+        }
     }
 
     @FXML
